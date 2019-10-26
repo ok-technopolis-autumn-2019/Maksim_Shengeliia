1ca6dd0009a173e706d8ceee7f9ac855da252733
Maksim Shengeliia <maksim.shengelia@mail.ru>
2019-10-26 10:31:39 +0300

    edit css #3

--- index.js
+++ index.js
@@ -3,8 +3,8 @@ const path = require('path');
 const PORT = process.env.PORT || 5000;
 
 express()
-    .use(express.static(path.join(__dirname, 'styles')))
-    .set('views', path.join(__dirname, 'views'))
-    .set('view engine', 'ejs')
-    .get('/', (req, res) => res.render('index'))
+    .use(express.static(path.join(__dirname, 'static')))
+    .get('/', function(req, res) {
+        res.sendFile(path.join(__dirname, 'static/views/index.html'))
+    })
     .listen(PORT, () => console.log(`Listening on ${ PORT }`));
